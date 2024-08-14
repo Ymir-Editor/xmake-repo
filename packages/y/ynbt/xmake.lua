4c0e0a7eed97d0ffbb139582dd6c79be0acefd9b
Duckos-Mods <businwes@hotmail.com>
2024-08-14 13:23:09 +0100

    Test

--- packages/y/ynbt/xmake.lua
+++ packages/y/ynbt/xmake.lua
@@ -6,6 +6,7 @@ package("ynbt")
 
     add_urls("https://github.com/Ymir-Editor/YNBT.git")
     add_versions("1.0", "9bb4b59214607ff9bd269c6e3885c067fe1369bd")
+    add_versions("1.1", "82a5585302bb3f8211b76b93719496accab8bbf9")
 
     on_install(function (package)
         local configs = {}
@@ -15,3 +16,11 @@ package("ynbt")
         import("package.tools.xmake").install(package, configs)
     end)
 
+    on_test(function (package)
+        assert(
+            package:has_cxxfuncs(
+                "YNBT::TagFromId",
+                {includes = "YNBT/ynbt.hpp"}
+            )
+        )
+    end)
