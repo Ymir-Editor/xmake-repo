c8ef4a5ddcc427fcf261f1a3dbaae8124ab45a83
Duckos-Mods <businwes@hotmail.com>
2024-08-17 11:37:33 +0100

    testPush

--- packages/y/ynbt/xmake.lua
+++ packages/y/ynbt/xmake.lua
@@ -1,20 +1,22 @@
 package("ynbt")
     set_description("YNBT is a cross platform NBT ser/de library for C++23")
     set_license("MIT")
+    add_urls("https://github.com/Ymir-Editor/YNBT.git")
+
+    add_version("1.3", "7184803d593a0399884f0d12644f760c5ccbdf17")
+
     add_deps("abseil")
     add_deps("zlib")
-
-    add_urls("https://github.com/Ymir-Editor/YNBT.git")
-    add_versions("1.0", "9bb4b59214607ff9bd269c6e3885c067fe1369bd")
-    add_versions("1.2", "25731f642952a06ce77b69d74803668267646091")
-
     on_install(function (package)
         local configs = {}
         if package:config("shared") then
             configs.kind = "shared"
         end
         import("package.tools.xmake").install(package, configs)
-    end)
+    end
 
     on_test(function (package)
+        assert(has_cxxtypes("YNBT::NBTFile", {includes = "ynbt/ynbt.hpp"}))
     end)
+
+    
