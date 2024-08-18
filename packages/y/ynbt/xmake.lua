e145b3214fe69aacea109649d1482d85794c07eb
Duckos-Mods <businwes@hotmail.com>
2024-08-18 21:46:46 +0100

    major

--- packages/y/ynbt/xmake.lua
+++ packages/y/ynbt/xmake.lua
@@ -17,7 +17,12 @@ package("ynbt")
     end)
    
     on_test(function (package)
-        assert(package:has_cxxfuncs("YNBT::TagFromId", {includes = "ynbt/ynbt.hpp", configs = {languages="cxx23"}}))
+        assert(package:check_cxxsnippets({test = [[
+            auto tag = YNBT::TagFromId(0);
+            if (tag.index() == 0)
+                return true;
+            return false;
+        ]]}, {includes = "ynbt/ynbt.hpp", configs = {languages="cxx23"}}))
     end)
 
     
